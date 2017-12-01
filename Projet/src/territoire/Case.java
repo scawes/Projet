c7767375a6a5682c6ff94535a857f49b2662900f
thomas <thomas@Fixe-Scawes.home>
2017-12-01 22:55:41 +0100

    modif mineur

--- Projet/src/territoire/Case.java
+++ Projet/src/territoire/Case.java
@@ -1,16 +1,31 @@
 package territoire;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import fourmi.Fourmi;
 import rapports.Rapport;
 import rapports.Trace;
 
 public class Case implements Trace{
 
 	Position position;
+	List<Fourmi> fourmiPresente;
 	
-	Case(Position position){
-		this.position=position;
+	Case(Position position) {
+		this.position = position;
+		fourmiPresente = new ArrayList<Fourmi>();
 	}
 
+	public void ajouterEntite(Fourmi fourmi){
+		fourmiPresente.add(fourmi);
+	}
+	
+	public void supprimerEntite(Fourmi fourmi){
+		fourmiPresente.remove(fourmi);
+	}
+	
+	
 	@Override
 	public void trace(Rapport rapport) {
 		rapport.traceForFourmiliere(this);
