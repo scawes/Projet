7d79e5b2e7aaa2b3f0ae65a24e99f9ee4347c90b
personlo <personlo@172.18.1.125>
2017-12-05 17:23:49 +0100

    Commentaire adulte

--- Projet/src/fourmi/etat/Adulte.java
+++ Projet/src/fourmi/etat/Adulte.java
@@ -33,15 +33,26 @@ public class Adulte extends Etat implements Trace {
 
 		role.evenement();
 	}
+	/*
+	 * Renvoi un boolean
+	 * Vrai : duree du trajet dépasse le temps limite
+	 * Faux : Il reste du temps en exterieur
+	 */
 	
 	public boolean dureetrajetLimite() {
-	  if(tempsDehors>600) {
-	    return true;
-	  }
-	  else {
-	    tempsDehors++;
-	    return false;
-	  }
+        	if(this.getPosition()!=this.getPositionsFourmiliere()) {  // vérification que la fourmi sois bien a l'exterieur
+          	  if(tempsDehors>600) {// 600 correspond a 10*60 minutes
+          	    return true;
+          	  }
+          	  else {
+          	    tempsDehors++;
+          	    return false;
+          	  }
+        	}
+        	else {
+        	  tempsDehors=0;
+        	  return false;
+        	}
 	}
 	
 	
@@ -52,29 +63,35 @@ public class Adulte extends Etat implements Trace {
 		role.trace(rapport);
 	}
 	
+	/*
+	 * Cette méthode augmente l'appetit de la fourmi dans le temps
+	 */
 	public void incrementeAppetit() {
 	  
 	  if(this.timerNourriture ==0) {
 	    appetit += (this.fourmi.getPoid()/180);
-	    timerNourriture =60;
+	    timerNourriture =60;// 60 correspond à 60 minutes
 	  }
 	  else {
 	    this.timerNourriture--;
 	  }
 	}
-	
+	/*
+	 * Cette methode represente l'action de se nourir de la fourmi
+	 */
 	public void manger() {
 	  int j =0;
 	  double nourriture;
-	  for (int i = 0 ; i< this.getPositionsFourmiliere().size(); i++) {
+	  for (int i = 0 ; i< this.getPositionsFourmiliere().size(); i++) { //verification de la position de la fourmi
 	    if(this.getPosition().equals(this.getPositionsFourmiliere().get(i))) {
-	      while(j <this.getProiePresnte().size() && appetit !=0) {
-	        if(appetit>this.getProiePresnte().get(j).getPoid()) {
+	      while(j <this.getProiePresnte().size() && appetit !=0) {// parcours des proies disponible
+	        if(appetit>this.getProiePresnte().get(j).getPoid()) {// la proie ne possede pas assez de poid et sera donc manger integralement
 	          nourriture = this.getProiePresnte().get(j).getPoid();
 	          this.getProiePresnte().get(j).decrementePoid(nourriture);
 	          appetit -=nourriture;
+	          j++;
 	        }
-	        else {
+	        else {//la proie peut satisfaire l'appetit de la fourmi et se vois donc retirer autant de poid que l'appetit
 	          nourriture = appetit;
 	          this.getCase().getProies().get(j).decrementePoid(nourriture);
                   appetit -=nourriture;
