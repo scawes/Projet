2482e102c62edf15d39f544fa9f1d79a7b8d8296
louarn <louarn@172.28.202.210>
2017-12-05 09:31:08 +0100

    robin

--- Projet/src/fourmi/tache/RetourFourmiliere.java
+++ Projet/src/fourmi/tache/RetourFourmiliere.java
@@ -1,28 +1,7 @@
 package fourmi.tache;
 
 import fourmi.role.Role;
-import territoire.Case;
 
 public class RetourFourmiliere extends Tache{
-
-	public RetourFourmiliere(Role role) {
-		super(role);
-	}
-	
-	public Case getNextCase() {
-		
-		Case nextCase;
-		
-		for(this.role.getEtat().getFourmi()) {}
-		
-		return null;
-		
-	}
-
-	@Override
-	public void evenement() {
-
-		
-	}
 
 }
