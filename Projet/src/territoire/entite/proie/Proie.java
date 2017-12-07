b0a8db3616d8c7d6bbeb9a1b1f153e4df1a83c14
louarn <louarn@172.28.202.210>
2017-12-07 09:47:42 +0100

    commentaiare

--- Projet/src/territoire/entite/proie/Proie.java
+++ Projet/src/territoire/entite/proie/Proie.java
@@ -12,74 +12,98 @@ import territoire.zone.deplacement.DeplacementProie;
 
 public class Proie implements Trace {
 
+	/*
+	 * Attributs
+	 */
+
 	Territoire territoire;
 	Etat etat;
 	DeplacementProie deplacement;
-	  
 
-	  double poid;
-	  int fraicheur;//a enlever
-	  private final double poidMin = 1.5;
-	  private final double poidMax = 5;
-	  
-	  public Proie(Territoire territoire,Position position) {
-		  this.territoire = territoire;
-		  deplacement = new DeplacementProie(this, position);
-		  etat = new Vivant(this);
-		  poid = (poidMin + (double)Math.random()* ((poidMax-poidMin)+1));
+	double poid;
+	private final double poidMin = 1.5;
+	private final double poidMax = 5;
+
+	/**
+	 * Constructeur de proie.
+	 * 
+	 * @param territoire
+	 *            la proie connait le territoire
+	 * @param position
+	 *            La position de la proie est initiliser
+	 */
+	public Proie(Territoire territoire, Position position) {
+		this.territoire = territoire;
+		deplacement = new DeplacementProie(this, position);
+		etat = new Vivant(this);
+		poid = (poidMin + (double) Math.random() * ((poidMax - poidMin) + 1));
+
+	}
+
+	/*
+	 * Getteurs et Setteurs
+	 */
 	
-	  }
-	  
-	  public Deplacement getDeplacement() {
-	    return this.deplacement;
-	  }
-	  
-	  public Territoire getTerritoire() {
-		    return this.territoire;
-	  }
-	  
-	  public boolean isVivant() {
-		  return etat.isVivant();
-	  }
-	  
-	  public Etat getVivant() {
-		  return etat.getVivant();
-	  }
-	  
-	  public Etat getEtat() {
-		  return etat;
-	  }
-	  
-	  public void mortProie() {
-		  etat = new MortProie(this);
-	  }
-	   
-	  public double getPoid() {
-	    return this.poid;
-	  }
-	  
-	  public double decrementePoid(double manger) {
-		  if(this.poid-manger<0){
-			  manger=this.poid;
-		  }
-	    this.poid -= manger;
-	    return manger;
-	  }
-	  
-	  public void evenement() {
-		  etat.evenement();
-	  }
+	public Deplacement getDeplacement() {
+		return this.deplacement;
+	}
+
+	public Territoire getTerritoire() {
+		return this.territoire;
+	}
+
+	public boolean isVivant() {
+		return etat.isVivant();
+	}
+
+	public Etat getVivant() {
+		return etat.getVivant();
+	}
+
+	public Etat getEtat() {
+		return etat;
+	}
+
+	public void mortProie() {
+		etat = new MortProie(this);
+	}
+
+	public double getPoid() {
+		return this.poid;
+	}
+
+	/*
+	 * Methodes
+	 */
+	
+	/**
+	 * 
+	 * @param manger
+	 * @return
+	 */
+	public double decrementePoid(double manger) {
+		if (this.poid - manger < 0) {
+			manger = this.poid;
+		}
+		this.poid -= manger;
+		return manger;
+	}
+
+	public void evenement() {
+		etat.evenement();
+	}
 
 	@Override
 	public void trace(Rapport rapport) {
 		etat.trace(rapport);
 	}
-	  
-	  /*public void deces() {
 
-	    this.enVie= false;
-	  }*/
-	  /*public void decrementeFraicheur() {
-	    fraicheur--;
-  }*/
+	/*
+	 * public void deces() {
+	 * 
+	 * this.enVie= false; }
+	 */
+	/*
+	 * public void decrementeFraicheur() { fraicheur--; }
+	 */
 }
